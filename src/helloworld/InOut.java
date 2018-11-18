707aa2b1ab1490395be030707b614a4ebc4e1dce
maenttang <36221352+maenttang@users.noreply.github.com>
2018-11-18 20:44:36 +0900

    github test

---
+++ src/helloworld/InOut.java
@@ -0,0 +1,36 @@
+package helloworld;
+
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+public class InOut {
+
+	public static void main(String[] args) {
+
+		String massage;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("문자입력");
+		
+		massage = in.nextLine();
+		
+		System.out.println(massage);
+				
+				
+				
+				
+				
+		
+//		Scanner scan = new Scanner(System.in); 
+//		
+//		System.out.println("문자를 입력하세요");
+//		
+//		massage = scan.nextLine();
+//
+//		System.out.println(massage);
+
+	}
+
+}
